7fce143d1eae9d901ac45b4a5fb35cad84a05255
Shaun <scpour@gmail.com>
2024-12-17 23:04:18 -0500

    rewrote to allow for user inputted numbers

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,39 @@
 import java.text.DecimalFormat;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator(1.0, 2.0);
+        Calculator();
     }
 
-    static void Calculator(double num1,double num2) {
-        final DecimalFormat df = new DecimalFormat("0.00");
+    static void Calculator() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("First number:");
+        double num1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Second number:");
+        double num2 = Integer.parseInt(scanner.nextLine());
+
+        final DecimalFormat df = new DecimalFormat("0.00");
+
         System.out.println("Choose your operation type (add subtract, multiply or divide):");
         String operation = scanner.nextLine();
 
         switch (operation) {
             case "add" -> {
-                double finalVal = num1 + num2;
+                System.out.println("First value: ");
+                double finalVal = Double.parseDouble(String.valueOf(num1 + num2));
                 System.out.println(finalVal);
             }
             case "subtract" -> {
-                double finalVal = num1 - num2;
+                double finalVal = Double.parseDouble(String.valueOf(num1 - num2));
                 System.out.println(finalVal);
             }
             case "multiply" -> {
-                double finalVal = num1 * num2;
+                double finalVal = Double.parseDouble(String.valueOf(num1 * num2));
                 System.out.println(finalVal);
             }
             case "divide" -> {
-                double finalVal = num1 / num2;
+                double finalVal = Double.parseDouble(String.valueOf(num1 / num2));
                 System.out.println(finalVal);
             }
             case null, default -> System.out.println("Invalid operation.");
